0c0fc87a4884ad34aa004784e734a2970a9f3086
Sylvester <25aima46@kristujayanti.com>
2026-03-28 12:01:06 +0530

    fixed port issue

--- backend/server.js
+++ backend/server.js
@@ -65,7 +65,7 @@ app.get("/orders", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 10000;
+const PORT = process.env.PORT || 3306;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
